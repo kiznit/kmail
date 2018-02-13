03007013507d85c23991ec55437a0347a6471d01
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-02-12 17:04:35 -0800

    Flesh out <Html>

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 
 
-const Html = ({ children, title, appState }) => (
-    <html>
+const Html = ({ title, description, appState, children }) => (
+    <html className="no-js" lang="en">
         <head>
-            <meta content="charset=utf-8" />
-             <title>{title}</title>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico?v=1" />
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=1" />
         </head>
-        <body style={{ backgroundColor: '#eee' }}>
+        <body>
             <h1>This is the Html component</h1>
-            <div id='react-root' dangerouslySetInnerHTML={{ __html: renderToString(children) }} />
-            <script dangerouslySetInnerHTML={{ __html: `window.INITIAL_APP_STATE=${JSON.stringify(appState)};` }} />
+            <div id='react-root' dangerouslySetInnerHTML={{__html: renderToString(children)}} />
+            <script dangerouslySetInnerHTML={{__html: `window.INITIAL_APP_STATE=${JSON.stringify(appState)};`}} />
             <script src="/js/bundle.js"></script>
         </body>
     </html>
@@ -20,13 +25,15 @@ const Html = ({ children, title, appState }) => (
 
 
 Html.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
     appState: PropTypes.object,
+    children: PropTypes.node.isRequired,
+    description: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
 };
 
 
 Html.defaultProps = {
+    description: '',
     title: 'Untitled',
 };
 
