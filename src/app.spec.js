68922af64642ad73d0949d1971bc02b89f28e7bd
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-03-18 23:51:08 -0700

    Login and CSRF unit tests.

--- src/app.spec.js
+++ src/app.spec.js
@@ -3,8 +3,10 @@ import app from './app';
 
 
 // Some regexs to validate cookies
-const COOKIE_CSRF = /^kmail.session=[\w\-]{24}; Path=\/; HttpOnly; SameSite=Strict$/;
-const COOKIE_SESSION = /^kmail.auth=s%3A[\w\-]{32}\.[\w%]{43,}; Path=\/; HttpOnly; SameSite=Strict$/;
+const RE_COOKIE_CSRF = /^kmail.session=[\w\-]{24}; Path=\/; HttpOnly; SameSite=Strict$/;
+const RE_COOKIE_SESSION = /^kmail.auth=s%3A[\w\-]{32}\.[\w%]{43,}; Path=\/; HttpOnly; SameSite=Strict$/;
+const RE_COOKIE_SESSION_DELETE = /kmail.auth=; Path=\/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Strict/;
+
 
 // There is a bug in Jest where it doesn't merge multiple 'set-cookie' headers properly.
 // Instead of having an array of cookies, we end up with an single-element list that
@@ -36,17 +38,20 @@ const getCookies = res => {
     return cookies[0].split(',');
 };
 
+// Extract the CSRF token from the HTML response
+const getCsrfToken = res => res.text.match(/window\._csrfToken = '([\w\-]{36})';/)[1];
 
-describe('app', () => {
 
-    test('serves static files', () => {
+describe("app", () => {
+
+    test("serves static files", () => {
         return request(app)
             .get('/apple-touch-icon.png')
             .expect(200);
     });
 
 
-    test('responds to /', () => {
+    test("responds to /", () => {
         return request(app)
             .get('/')
             .expect(200)
@@ -59,11 +64,103 @@ describe('app', () => {
                 //  - We do not expect a session cookie (the user is not logged in yet)
                 const cookies = getCookies(res);
                 expect(cookies).to.be.an('array').and.have.length(1);
-                expect(cookies).to.include.something.that.match(COOKIE_CSRF);
+                expect(cookies).to.include.something.that.match(RE_COOKIE_CSRF);
             });
     });
 });
 
 
-//todo: each page load has a different token, even for the same user (COOKIE_CSRF)
-//todo: csrf test with two agents exchanging their token
+describe("login", () => {
+
+    const agent = request.agent(app);
+    let csrfToken;
+
+    test("user loads page", () => {
+        return agent.get('/')
+            .expect(200)
+            .expect('set-cookie', RE_COOKIE_CSRF)
+            .expect(res => {
+                // We must scrub the csrfToken from the HTML
+                csrfToken = getCsrfToken(res);
+            });
+    });
+
+    test("invalid user can't log in", () => {
+        return agent
+            .post('/api/login')
+            .send({ username: 'admin2', password: '12345' })
+            .set('X-CSRF-Token', csrfToken)
+            .expect(401);
+    });
+
+    test("user can log in", () => {
+        return agent
+            .post('/api/login')
+            .send({ username: 'admin', password: '1234' })
+            .set('X-CSRF-Token', csrfToken)
+            .expect(200)
+            .expect('set-cookie', RE_COOKIE_SESSION);
+    });
+
+    test("user can log out", () => {
+        return agent
+            .post('/api/logout')
+            .set('X-CSRF-Token', csrfToken)
+            .expect(200)
+            .expect('set-cookie', RE_COOKIE_SESSION_DELETE);
+    });
+});
+
+
+describe("CSRF", () => {
+    const agent1 = request.agent(app);
+    const agent2 = request.agent(app);
+    let csrfToken1;
+    let csrfToken2;
+
+    test("users can't login without a token", async () => {
+        await agent1
+            .post('/api/login')
+            .send({ username: 'admin', password: '1234' })
+            .expect(403);
+    });
+
+    test("users have different CSRF tokens", async () => {
+        const res1 = await agent1.get('/');
+        const res2 = await agent2.get('/');
+
+        csrfToken1 = getCsrfToken(res1);
+        csrfToken2 = getCsrfToken(res2);
+
+        expect(csrfToken2).to.not.equal(csrfToken1);
+    });
+
+    test("users can't swap tokens", async ()=> {
+        await agent1
+            .post('/api/login')
+            .send({ username: 'admin', password: '1234' })
+            .set('X-CSRF-Token', csrfToken2)
+            .expect(403);
+
+        await agent2
+            .post('/api/login')
+            .send({ username: 'admin', password: '1234' })
+            .set('X-CSRF-Token', csrfToken1)
+            .expect(403);
+    });
+
+
+    test("users can login with their tokens", async () => {
+        await agent1
+            .post('/api/login')
+            .send({ username: 'admin', password: '1234' })
+            .set('X-CSRF-Token', csrfToken1)
+            .expect(200);
+
+        await agent2
+            .post('/api/login')
+            .send({ username: 'admin', password: '1234' })
+            .set('X-CSRF-Token', csrfToken2)
+            .expect(200);
+    });
+});
