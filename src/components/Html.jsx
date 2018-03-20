dc136da23acedfa8852c79baa1e4c3e6f284dc72
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-03-19 21:44:36 -0700

    No robots please.

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -43,6 +43,7 @@ const Html = ({ title, description, scripts, appState, csrfToken, children }) =>
             <head>
                 <meta charSet="utf-8" />
                 <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta name="robots" content="noindex,nofollow" />
                 <title>{title}</title>
                 <meta name="description" content={description} />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
