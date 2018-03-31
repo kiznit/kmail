737d8fe428ed74cae4beab1190a363ea42433dff
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-03-31 02:21:30 +0100

    Add chai-as-promised.

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,15 +1,21 @@
 import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import chaiThings from 'chai-things';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 
-
+// Configure Chai
 chai.config.includeStack = true;
+chai.use(chaiAsPromised);
 chai.use(chaiThings);
 chai.use(sinonChai);
 
+// Keep a reference to Jest's expect() so that we can use it for snapshot testing.
+global.jestExpect = global.expect;
+
+// I prefer Chai over Jest's assertions, so let's use that.
 global.assert = chai.assert;
 global.expect = chai.expect;
-global.sinon = sinon;
 
-global.jestExpect = global.expect;
+// I prefer Sinon over Jest's mocking capabilities, so let's use that.
+global.sinon = sinon;
