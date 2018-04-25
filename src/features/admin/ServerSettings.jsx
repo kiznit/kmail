89e27f8afb89c82d093862ef822f7cbc1da8e440
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-04-25 18:10:16 +0100

    Create feature/settings/category

--- src/features/admin/ServerSettings.jsx
+++ src/features/admin/ServerSettings.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 
 import { FormControl } from 'material-ui/Form';
 import { InputLabel } from 'material-ui/Input';
-import List, { ListItem } from 'material-ui/List';
 import { MenuItem } from 'material-ui/Menu';
-import Paper from 'material-ui/Paper';
 import Select from 'material-ui/Select';
-import { withStyles } from 'material-ui/styles';
 import TextField from 'material-ui/TextField';
-import Typography from 'material-ui/Typography';
+
+import SettingsCategory from 'features/settings/category';
 
 
 const styles = theme => ({
@@ -37,43 +35,28 @@ class ServerSettings extends React.Component {
     };
 
     render() {
-        const { classes, title } = this.props;
+        const { title } = this.props;
         const { url, port, security } = this.state;
 
         return (
-            <div>
-                <Typography variant="title" className={classes.title}>
-                    {title}
-                </Typography>
-
-                <List component="nav">
-                    <Paper>
-                        <ListItem>
-                            <TextField label="Server" value={url} onChange={this.handleChange('url')} />
-                        </ListItem>
-                        <ListItem>
-                            <TextField label="Port" type="number" min={0} max={65535} value={port} onChange={this.handleChange('port')} />
-                        </ListItem>
-                        <ListItem>
-                            <FormControl>
-                                <InputLabel htmlFor="controlled-open-select-imap">Secure</InputLabel>
-                                <Select value={security} label="Security" inputProps={{ name: 'security', id: 'controlled-open-select-imap' }} onChange={this.handleChange('security')}>
-                                    <MenuItem value={0}>None</MenuItem>
-                                    <MenuItem value={1}>SSL/TLS</MenuItem>
-                                    <MenuItem value={2}>STARTTLS</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </ListItem>
-                    </Paper>
-                </List>
-            </div>
+            <SettingsCategory title={title}>
+                <TextField label="Server" value={url} onChange={this.handleChange('url')} />
+                <TextField label="Port" type="number" min={0} max={65535} value={port} onChange={this.handleChange('port')} />
+                <FormControl>
+                    <InputLabel htmlFor="controlled-open-select-imap">Secure</InputLabel>
+                    <Select value={security} label="Security" inputProps={{ name: 'security', id: 'controlled-open-select-imap' }} onChange={this.handleChange('security')}>
+                        <MenuItem value={0}>None</MenuItem>
+                        <MenuItem value={1}>SSL/TLS</MenuItem>
+                        <MenuItem value={2}>STARTTLS</MenuItem>
+                    </Select>
+                </FormControl>
+            </SettingsCategory>
         );
     }
 }
 
 
 ServerSettings.propTypes = {
-    classes: PropTypes.shape({}).isRequired,
     port: PropTypes.number,
     title: PropTypes.string.isRequired,
     url: PropTypes.string,
@@ -86,4 +69,4 @@ ServerSettings.defaultProps = {
 };
 
 
-export default withStyles(styles)(ServerSettings);
+export default ServerSettings;
---
+++ src/features/settings/category.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import List, { ListItem } from 'material-ui/List';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+
+
+const styles = theme => ({
+    title: {
+        margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit}px`,
+    },
+});
+
+
+const Category = ({ children, classes, title }) => (
+    <div>
+        <Typography variant="title" className={classes.title}>
+            {title}
+        </Typography>
+        <List component="nav">
+            <Paper>
+                {
+                    React.Children.map(children, child => (
+                        <ListItem>
+                            { child }
+                        </ListItem>
+                    ))
+                }
+            </Paper>
+        </List>
+    </div>
+);
+
+
+Category.propTypes = {
+    children: PropTypes.node.isRequired,
+    classes: PropTypes.shape({}).isRequired,
+    title: PropTypes.string.isRequired,
+};
+
+
+export default withStyles(styles)(Category);
+
