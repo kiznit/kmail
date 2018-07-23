fcee4f5c680d75a7a1b67457043c555663542c8c
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-07-23 16:07:29 -0700

    Clean up TextInput

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 
-
 //TODO: replace validation rules this with Yup
 export const required = value => (value ? null : 'This field is required');
 
@@ -21,111 +20,18 @@ export const range = (min, max) => value => {
 };
 
 
-/*
-    Provides validation on top of TextField
-*/
-class TextInput extends React.PureComponent {
-    constructor(props) {
-        super(props);
-
-        const { value } = props;
-
-        this.state = {
-            value,
-            touched: false,
-        };
-    }
-
-
-    componentDidMount() {
-        //this.context.form.register(this);
-    }
-
-
-    componentWillUnmount() {
-        //this.context.form.unregister(this);
-    }
-
-
-    validate(submitting) {
-        const { validate } = this.props;
-
-        if (!validate) {
-            return true;
-        }
-
-        const { value } = this.state;
-
-        let error = null;
-        const rules = Array.isArray(validate) ? validate : [validate];
-
-        rules.forEach(rule => {
-            error = error || rule(value);
-        });
-
-        this.setState({
-            error,
-            touched: this.state.touched || submitting,
-        });
-
-        return error === null;
-    }
-
-
-    handleBlur = event => {
-        const { value } = event.target;
-
-        this.setState({ value, touched: true }, () => {
-            //this.context.form.validate();
-        });
-
-        const { onBlur } = this.props;
-
-        if (onBlur) {
-            onBlur(event);
-        }
-    }
-
-
-    handleChange = event => {
-        const { value } = event.target;
-
-        this.setState({ value }, () => {
-            //this.context.form.validate();
-        });
-
-        const { onChange } = this.props;
-
-        if (onChange) {
-            onChange(event);
-        }
-    }
-
-
-    render() {
-        const { error, helperText, touched, validate, ...props } = this.props;
-
-        return (
-            <TextField
-                {...props}
-                error={!!error && touched}
-                helperText={(touched && error) || helperText || ' '} // The space prevents the TextField from changing height on errors}
-                onBlur={this.handleBlur}
-                onChange={this.handleChange}
-                value={this.state.value}
-            />
-        );
-    }
-}
-
-
-// TextInput.contextTypes = {
-//     form: PropTypes.shape({
-//         register: PropTypes.func.isRequired,
-//         unregister: PropTypes.func.isRequired,
-//         validate: PropTypes.func.isRequired,
-//     }).isRequired,
-// };
+const TextInput = ({
+    error,
+    helperText,
+    touched,
+    ...props
+}) => (
+    <TextField
+        {...props}
+        error={!!error && touched}
+        helperText={(touched && error) || helperText || ' '} // The space prevents the TextField from changing height on errors}
+    />
+);
 
 
 TextInput.propTypes = {
@@ -133,21 +39,16 @@ TextInput.propTypes = {
     fullWidth: PropTypes.bool,
     helperText: PropTypes.node,
     margin: PropTypes.string,
-    onBlur: PropTypes.func,
-    onChange: PropTypes.func,
+    onBlur: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
     touched: PropTypes.bool,
-    validate: PropTypes.oneOfType([
-        PropTypes.func,
-        PropTypes.arrayOf(PropTypes.func),
-    ]),
-    value: PropTypes.string,
+    value: PropTypes.string.isRequired,
 };
 
 
 TextInput.defaultProps = {
     fullWidth: true,
     margin: 'dense',
-    value: '',
 };
 
 
