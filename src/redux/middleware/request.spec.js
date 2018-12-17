21ce4a329ec9d25cc454e9ec5f631e2ceb06420a
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-12-17 05:32:45 +0000

    One more test for request middleware (failed request) to bring coverage to 100%.

--- src/redux/middleware/request.spec.js
+++ src/redux/middleware/request.spec.js
@@ -73,4 +73,32 @@ describe('Redux request middleware', () => {
             bonus: 'property',
         });
     });
+
+
+    test('Transforms failed request action into promise action', async () => {
+        const response = {
+            ok: false,
+            status: 404,
+            statusText: 'Not found',
+        };
+        global.fetch = (endpoint, options) => Promise.resolve(response);
+
+        return dispatch({
+            type: 'ACTION',
+            request: {
+                endpoint: '/api/awesome',
+            },
+            bonus: 'property',
+        })
+            .then(() => { throw new Error('Unexpected'); })
+            .catch(err => {
+                expect(err.message).to.equal('Status 404: Not found');
+                expect(baseDispatch).to.have.been.calledOnce;
+                expect(baseDispatch).to.have.been.calledWithExactly({
+                    type: 'ACTION',
+                    promise,
+                    bonus: 'property',
+                });
+            });
+    });
 });
