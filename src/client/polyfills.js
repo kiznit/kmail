dd69f60971b8e136a46c1fc7408dc7c7ceb5d2e3
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-12-18 05:51:43 +0000

    Add Intl polyfill (with only en locale for now).

--- src/client/polyfills.js
+++ src/client/polyfills.js
@@ -1,16 +1,23 @@
 const loadPolyfills = () => {
     const promises = [];
 
+    // fetch
     if (!('fetch' in window)) {
         promises.push(import(/* webpackChunkName: "fetch-polyfill" */ 'isomorphic-fetch'));
     }
 
+    // ES6
     if (!('startsWith' in String.prototype
         && 'endsWith' in String.prototype
         && 'includes' in Array.prototype
         && 'assign' in Object
         && 'keys' in Object)) {
         promises.push(import(/* webpackChunkName: "es6-polyfills" */ '@babel/polyfill'));
+    }
+
+    // Intl
+    if (!('Intl' in window)) {
+        promises.push(import(/* webpackChunkName: "intl-polyfill" */ './intl'));
     }
 
     return Promise.all(promises);
