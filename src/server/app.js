84782c32568b7de113401bab1d8dc927a5220bb1
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-12-28 08:43:38 +0000

    Move /ping route before static content.

--- src/server/app.js
+++ src/server/app.js
@@ -12,14 +12,13 @@ const app = express();
 // Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
 app.use(helmet());
 
+// Simple /ping route - can be used by load balancers or deployment systems
+// to verify that the server is up and running.
+app.get('/ping', (req, res) => res.status(200).end());
 
 // Static content
 const publicPath = __TEST__ ? '../../public' : '../public';
 app.use('/', express.static(path.resolve(__dirname, publicPath)));
-
-// Simple /ping route - can be used by load balancers or deployment systems
-// to verify that the server is up and running.
-app.get('/ping', (req, res) => res.status(200).end());
 
 // Azure uses 'x-arr-ssl' instead of 'x-forwarded-proto', so fix that.
 app.use((req, res, next) => {
