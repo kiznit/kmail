869cebe7f2db095a3037744edd8bbf96bcbc9325
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-12-28 06:39:51 +0000

    Azure deployment support (App Service).

---
+++ index.js
@@ -0,0 +1,5 @@
+// Some platforms (IIS, Azure) won't allow you to set the working directory.
+// This file allows us to launch iisnode from the root directory, which
+// is what we want as our working directory.
+
+require('./dist/server/server');
